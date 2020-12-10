bdb183e08dfe564813193c12ffabf776972c7e0b
MF1986 <68443589+MF1986@users.noreply.github.com>
2020-12-10 23:25:29 +0100

    Create 04.even_odd_number.py

---
+++ exercises/04.even_odd_number.py
@@ -0,0 +1,8 @@
+#input a number
+number = int(input("Which number do you want to check? "))
+
+#check if number modulo 2 is equal to 0 or 1 and print accordingly.
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("this is an odd number.")
