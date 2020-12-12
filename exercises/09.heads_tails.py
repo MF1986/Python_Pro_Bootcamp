cf003d635f1f3ae801708b0fa78d92e9b4a55281
MF1986 <68443589+MF1986@users.noreply.github.com>
2020-12-12 11:03:51 +0100

    Create 09.heads_tails.py

---
+++ exercises/09.heads_tails.py
@@ -0,0 +1,11 @@
+#Import module random
+import random
+
+#Random number between 0 and 1
+random_integer = random.randint(0, 1)
+
+#Print statement
+if random_integer == 1:
+  print("Heads")
+else:
+  print("tails")
