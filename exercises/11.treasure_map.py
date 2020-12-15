c38cec4e477fe84beb1282d10a1fd797cafca3b8
MF1986 <68443589+MF1986@users.noreply.github.com>
2020-12-15 13:31:05 +0100

    Update 11.treasure_map.py

--- exercises/11.treasure_map.py
+++ exercises/11.treasure_map.py
@@ -1,10 +1,12 @@
+#ROW 1 = 11, 12, 13
+#ROW 2 = 21, 22, 23
+#ROW 3 = 31, 32, 33
+
 row1 = ["⬜️","⬜️","⬜️"]
 row2 = ["⬜️","⬜️","⬜️"]
 row3 = ["⬜️","⬜️","⬜️"]
 
 map = [row1, row2, row3]
-
-print(f"{row1}\n{row2}\n{row3}")
 
 position = input("Where do you want to put the treasure? ")
 
